ccb34b77fb005c231b7f978d8879c1f95b8db545
Mitchel Cabuloy <mixxorz@gmail.com>
2017-01-11 17:41:25 +0800

    Configured logging

--- hello/settings.py
+++ hello/settings.py
@@ -129,3 +129,35 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format':
+            "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'WARN'
+        },
+        'apps': {
+            'handlers': ['console'],
+            'level': 'WARN'
+        },
+    }
+}
